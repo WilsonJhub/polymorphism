9c8e1c01807672949bf44f9d0b39084ae2b6655e
wilsonjhub <jasonstimphil@gmail.com>
2022-07-29 11:25:10 -0400

    I have added 2 classes that extends the parent Class Game and overrides its game() method.

---
+++ learningpolymorphism/Game.java
@@ -0,0 +1,10 @@
+package learningpolymorphism;
+
+public class Game {
+    private String name;
+    private String genre;
+
+    public void game() {
+        System.out.println("The game name will go here.");
+    }
+}
